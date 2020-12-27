cec8fd524a9a6d6dc140ff17c07a1265e4f9385c
ysski <yutasasaki2216@gmail.com>
2020-12-27 17:12:13 +0900

    Font Awesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import '@fortawesome/fontawesome-free/js/all';
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
