0aedf3d8df2d1797422448990e8fd2c9401b0d13
ysski <yutasasaki2216@gmail.com>
2020-12-28 10:50:58 +0900

    リスト編集機能の実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:edit, :update]
+
   def new
     @list = List.new
   end
@@ -12,8 +14,25 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+   
+    if @list.update(list_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def list_params
     params.require(:list).permit(:title).merge(user:current_user)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
